a7558cd3d2e0193d0604bf3b7e38b614a370001d
Kevin <kchild89@gmail.com>
2025-05-06 08:20:49 -0600

    ignored ios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'detail',
+    loadComponent: () => import('./tabs1/detail/detail.page').then( m => m.DetailPage)
+  },
+  {
+    path: 'detail',
+    loadComponent: () => import('./tabs1/detail/detail.page').then( m => m.DetailPage)
+  },
 ];
